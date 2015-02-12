db5d888094a110e4e989756f3cdf395c0ffec60c
nlbtoan <nlb.toan@gmail.com>
2015-02-12 04:26:22 +0000

    complete updating user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       
-      redirect_to user
+      #redirect_to user
+      redirect_back_or user
     else
       #create an error message
       #flash[:danger] = 'Invalid email/password combination' # Not quite right!
